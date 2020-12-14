64d2523cbb0debaa5e57cedd21719124da12fe02
waskor <48648846+waskor@users.noreply.github.com>
2020-12-14 19:28:25 +0300

    Create generateqr.py

---
+++ generateqr.py
@@ -0,0 +1,36 @@
+import qrcode
+import pandas as pd
+import cProfile
+
+def initialise():
+
+    global links, qr
+
+    filename = "grollz/links.csv"
+    links = pd.read_csv(filename)
+
+    qr = qrcode.QRCode(
+    None,
+    error_correction=qrcode.constants.ERROR_CORRECT_M,
+    box_size=10,
+    border=2,
+    )
+
+
+def generate_qr(i):
+    
+    global qrimg
+
+    link = links.values[i,1]
+    qr.add_data(link)
+    qr.make(fit=True)
+    qrimg = qr.make_image(fill_color="black", back_color="white")
+
+    qr.data_list.clear()
+
+
+# def main():
+#     initialise()
+#     generate_qr(0)
+
+#cProfile.run('main()')
