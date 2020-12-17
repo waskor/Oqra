8122eb86a7922ded4e7fff4d09648cd988cf2993
waskor <48648846+waskor@users.noreply.github.com>
2020-12-18 01:05:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from PIL import Image
 import placeqr as pqr
 import generateqr as gqr
 
+
 class External(QThread):
 
     countchange = pyqtSignal(int)
@@ -23,26 +24,29 @@ class External(QThread):
         self.amount = amount
 
     def run(self):
-        
+        start_time = time.time()
         self.find_squares(self.templatefile)
         self.place_qr_codes(self.templatefile, self.linksfile, self.amount)
+        print("--- %s seconds ---" % (time.time() - start_time))
 
     def find_squares(self, templatefile):
 
         #global squares_sort
         self.template = cv2.imread(self.templatefile)
+        self.width, self.height, self.channels = self.template.shape
         self.squares_list = []
         self.templategray = cv2.cvtColor(self.template, cv2.COLOR_BGR2GRAY)
         
         self.ret, self.thresh = cv2.threshold(self.templategray,127,255,cv2.THRESH_BINARY_INV)
 
-        self.contours, self.hierarchy = cv2.findContours(self.thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        self.contours, self.hierarchy = cv2.findContours(self.thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         for c in self.contours:    
 
             self.x,self.y,self.w,self.h = cv2.boundingRect(c)
+            self.ratio = self.w/self.h
 
-            if self.w>80:
+            if min(1.01,0.99) < self.ratio < max(1.01,0.99) and self.height/self.w<70:
                 
                 self.accuracy = 0.01*cv2.arcLength(c,True)
                 self.approx = cv2.approxPolyDP(c,self.accuracy,True)
@@ -67,12 +71,12 @@ class External(QThread):
         self.sq = 0
 
         for i in range(self.amount):
-            count+=100/self.amount
-            self.countchange.emit(count)
+            count+=101/self.amount
+            self.countchange.emit(int(count))
 
             if i % self.squares_sort.shape[0] == 0 and i != 0:
 
-                self.fileout = "grollz/stickers/templateqr_{}.png".format(self.page)
+                self.fileout = str(mainwindow.outputfolder) + "/templateqr_{}.png".format(self.page)
                 self.template_qr.save(self.fileout)
                 self.page += 1
                 self.sq = 0
@@ -94,7 +98,7 @@ class External(QThread):
                 gqr.qrimg = gqr.qrimg.resize((self.squares_sort[self.sq,3], self.squares_sort[self.sq,2]))
                 
                 self.template_qr.paste(gqr.qrimg, (self.squares_sort[self.sq,0], self.squares_sort[self.sq,1])) 
-                self.fileout = "grollz/stickers/templateqr_{}.png".format(self.page)
+                self.fileout = str(mainwindow.outputfolder) + "/templateqr_{}.png".format(self.page)
                 self.template_qr.save(self.fileout)
                 gqr.links.to_csv("links2.csv")
 
@@ -107,50 +111,91 @@ class External(QThread):
                 
                 self.sq += 1
 
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi("grollz/stiqr.ui",self)
         self.browse1.clicked.connect(self.browsepng)
         self.browse2.clicked.connect(self.browsecsv)
+        self.browse3.clicked.connect(self.browsefolder)
         self.checkboxall.stateChanged.connect(self.checkbox)
         self.checkboxcustom.stateChanged.connect(self.checkbox)
         self.runbutton.clicked.connect(self.execute)
         self.customline.setDisabled(True)
         self.progressBar.setValue(0)
         self.progressBar.setMaximum(100)
+        self.checkboxall.setDisabled(True)
+        self.checkboxcustom.setDisabled(True)
 
         
     def browsepng(self):
+
         filename = QFileDialog.getOpenFileName(self, 'Load template', "C:/", "PNG Files (*.png)")
         self.browsepath1.setText(filename[0])
 
         self.templatefile = filename[0]
 
+        if self.templatefile.endswith('.png'):
+            self.checkboxall.setDisabled(False)
+            self.checkboxcustom.setDisabled(False)
+            self.loadingline.setText('')
+        else:
+            self.loadingline.setText('Please select a PNG template file.')
+
     def browsecsv(self):
+        self.loadingline.setText('')
+
         filename = QFileDialog.getOpenFileName(self, 'Load links', "C:/", "CSV Files (*.csv)")
         self.browsepath2.setText(filename[0])
 
         self.linksfile = filename[0]
 
+        if self.linksfile.endswith('.csv'):
+            self.checkboxall.setDisabled(False)
+            self.checkboxcustom.setDisabled(False)
+            self.loadingline.setText('')
+        else:
+            self.loadingline.setText('Please select a correct links file.')
+
+
+    def browsefolder(self):
+        filename = QFileDialog.getExistingDirectory(self, 'Output folder', "C:/", QFileDialog.ShowDirsOnly)
+        self.browsepath3.setText(filename)
+        self.outputfolder = filename
+
+        
     def checkbox(self, state):
         if state == QtCore.Qt.Checked:
             if self.sender() == self.checkboxall:
+
                 self.checkboxcustom.setChecked(False)
+
                 try:
                     self.linksfile
                 except:
-                    print('no links')
+                    self.loadingline.setText('Please set links file first.')
+                    self.checkboxall.setChecked(False)
+
                 else:
                     
                     gqr.initialise(self.linksfile)
                     self.amount = gqr.links.shape[0]
-                    print(self.amount)
 
             if self.sender() == self.checkboxcustom:
-                print('custom')
+
                 self.checkboxall.setChecked(False)
-                self.customline.setDisabled(False)
+
+                try:
+                    self.linksfile
+                except:
+                    self.loadingline.setText('Please set links file first.')
+                    self.checkboxcustom.setChecked(False)
+                else:
+                    self.customline.setDisabled(False)
+
+
         else:
             self.customline.setDisabled(True)
 
@@ -162,15 +207,47 @@ class MainWindow(QMainWindow):
 
     def execute(self):
 
-        self.loadingline.setText('Generating...')
+        if not self.checkboxcustom.isChecked() and not self.checkboxall.isChecked():
+            self.loadingline.setText('Please select the amount of links.')
 
-        self.calc = External(self.templatefile, self.linksfile, self.amount)
-        self.calc.finished.connect(self.done)
-        self.calc.countchange.connect(self.onCountChanged)
-        self.calc.start()
+        try:
+            self.templatefile
+        except:
+            self.loadingline.setText('Please select template file.')
+        
+        else:
 
-        #pqr.main(self.templatefile, self.linksfile, self.amount)
-      
+            try:
+                self.linksfile
+            except:
+                self.loadingline.setText('Please select link file.')
+
+            else:
+
+                try:
+                    self.outputfolder
+                except:
+                    self.loadingline.setText('Please select output folder.')
+
+                else:
+                    
+                    try:
+                        self.amount
+                    except:
+                        self.loadingline.setText('Please select amount of links.')
+                    
+                    else:
+
+                        if self.checkboxcustom.isChecked():
+                            self.amount = int(self.customline.text())
+
+                        self.loadingline.setText('Generating...')
+                        self.calc = External(self.templatefile, self.linksfile, self.amount)
+                        self.calc.finished.connect(self.done)
+                        self.calc.countchange.connect(self.onCountChanged)
+                        self.calc.start()
+
+
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
